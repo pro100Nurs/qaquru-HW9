00a8e6935fa8629703d0f71dd93e2d9cfabc9c8b
pro100Nurs <nurs.qa@mail.ru>
2021-12-04 23:26:21 +0600

    updated selenide version

--- src/test/java/guru/qa/GoogleTests.java
+++ src/test/java/guru/qa/GoogleTests.java
@@ -1,8 +1,8 @@
 package guru.qa;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -11,11 +11,13 @@ public class GoogleTests {
 
     @Test
     void selenideSearchTest() {
-
+        // Открыть google
         open("https://google.com");
 
+        // Ввести Selenide в поиск
         $(byName("q")).setValue("Selenide").pressEnter();
 
-        $("#search").shouldHave(Condition.text("selenide.org"));
+        // Проверить, что Selenide появился в результатах поиска
+        $("#search").shouldHave(text("selenide.org"));
     }
 }
